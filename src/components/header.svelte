524446dda6dfbf9ff2d7ec28093bec6b5c98a206
Wesley <wesmcghee6372@gmail.com>
2023-11-14 21:42:07 -0600

    initialzied header

---
+++ src/components/header.svelte
@@ -0,0 +1,24 @@
+<header>
+  <h1>RTN</h1>
+  <div>
+    <ul>
+      <li>
+        <a href="/">fill</a>
+      </li>
+      <li>
+        <a href="/">fill</a>
+      </li>
+      <li>
+        <a href="/">fill</a>
+      </li>
+    </ul>
+  </div>
+</header>
+
+<style>
+  header {
+    height: 7vh;
+    width: 100%;
+    background-color: #747474;
+  }
+</style>
